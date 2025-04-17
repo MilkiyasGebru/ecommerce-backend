022eb88e03c81d298c3f6c79e4beadfa34b097f1
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-17 12:01:00 +0300

    Added the user routers

---
+++ routers/UserRouters.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import {login_user, register_user} from "../controllers/UserControllers.js";
+
+const user_router = express.Router();
+
+
+user_router.post("/register", register_user)
+user_router.post("/login", login_user)
