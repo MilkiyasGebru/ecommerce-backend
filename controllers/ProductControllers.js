9ee668ccdccafb942d3300cad2bd023005e5f1dd
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-17 20:55:30 +0300

    Added the add Product function and handled the query parameters in the getAllProducts

--- controllers/ProductControllers.js
+++ controllers/ProductControllers.js
@@ -2,30 +2,30 @@ import ProductModel from "../models/ProductModel.js";
 import productModel from "../models/ProductModel.js";
 
 const products = [
-    // {
-    //     product_name: "tires",
-    //     category: "Tire",
-    //     brand: "BMW",
-    //     price: 100,
-    // },
-    // {
-    //     product_name: "suspension",
-    //     category: "Suspension",
-    //     brand: "Tesla",
-    //     price: 210,
-    // },
-    // {
-    //     product_name: "car paint",
-    //     category: "Paint",
-    //     brand: "Toyota",
-    //     price: 53,
-    // },
-    // {
-    //     product_name: "Head Light",
-    //     category: "Light",
-    //     brand: "BMW",
-    //     price: 11.23,
-    // }
+    {
+        product_name: "tires",
+        category: "Tire",
+        brand: "BMW",
+        price: 100,
+    },
+    {
+        product_name: "suspension",
+        category: "Suspension",
+        brand: "Tesla",
+        price: 210,
+    },
+    {
+        product_name: "car paint",
+        category: "Paint",
+        brand: "Toyota",
+        price: 53,
+    },
+    {
+        product_name: "Head Light",
+        category: "Light",
+        brand: "BMW",
+        price: 11.23,
+    },
     {
         product_name: "engine",
         category: "Engine",
@@ -51,8 +51,15 @@ export const getTrendingProducts = async (req,res)=>{
 
 export const getAllProducts = async (req,res)=>{
 
-    const {category, brand} = req.query
-    const all_products = await ProductModel.find()
+    const {categories, brands} = req.query
+    const filter_options = {}
+    if (categories){
+        filter_options.category = categories.split(",")
+    }
+    if (brands){
+        filter_options.brand = brands.split(",")
+    }
+    const all_products = await ProductModel.find(filter_options)
     if(!all_products){
         return res.status(404).json({message:"No products found"})
     }
@@ -66,9 +73,16 @@ export const getProductsByCategory = async(req,res)=>{
     res.status(200).json(get_by_category)
 
 }
+
 export const getProductsByBrand = async(req,res)=>{
     const { brand } = req.query;
     const arr = brand.split(",");
     const get_by_brand = await ProductModel.find({brand: {$in:arr}})
     res.status(200).json(get_by_brand)
+}
+
+export const addProduct = async (req, res)=>{
+    const {product_name, brand, category, price, description} = req.body
+    await ProductModel.create({product_name, brand, category, price, product_description:description})
+    return res.status(201).json({message:"Product Added"})
 }
