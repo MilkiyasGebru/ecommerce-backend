f031bc125691423dd9da28fee846d563d2877238
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-19 10:53:00 +0300

    added the environment variable configuration

---
+++ config/env_var.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+export const ENV_VARS = {
+    PORT: process.env.PORT || 8080,
+    MONGODB_URI: process.env.MONGODB_URI,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    JWT_SECRET: process.env.JWT_SECRET,
+}
