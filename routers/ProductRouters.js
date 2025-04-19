6b4917a4c2d6f0d8644330a47f2e3d20a4851afb
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-19 08:05:10 +0300

    Added a multer middleware to handle file upload

--- routers/ProductRouters.js
+++ routers/ProductRouters.js
@@ -1,11 +1,26 @@
 import express from 'express';
+import multer from 'multer';
 import {
+    createProduct,
     getAllProducts,
     getLatestProducts, getProductsByBrand,
     getProductsByCategory,
     getTrendingProducts
 } from "../controllers/ProductControllers.js";
 
+// const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        // Specify the directory where you want to save the files
+        cb(null, './uploads/'); // Make sure this directory exists
+    },
+    filename: function (req, file, cb) {
+        // Define how the uploaded file should be named
+        const uniqueSuffix = Date.now() ;
+        cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
+    }
+});
+const upload = multer({ storage: storage });
 const product_router = express.Router();
 
 
@@ -14,5 +29,6 @@ product_router.get("/trending",getTrendingProducts)
 product_router.get("/",getAllProducts)
 product_router.get("/:category",getProductsByCategory)
 product_router.get("/brand/:brand",getProductsByBrand)
+product_router.post("/add",upload.single('photo'), createProduct)
 
 export default product_router;
