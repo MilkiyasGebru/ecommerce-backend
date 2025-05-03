5ff5ff571c6cd571f1200e6fec75e39a5c770f51
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-05-03 08:55:49 +0300

    fixed the cors options

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import user_router from "./routers/UserRouters.js";
 
 const app = express();
 const corsOptions = {
-    origin: "*", // Or an array of allowed origins
+    origin: ["https://autoshoopa.vercel.app/"], // Or an array of allowed origins
     methods:['GET', 'PUT', 'POST']  ,
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // If you need to handle cookies
