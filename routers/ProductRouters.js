85dab3b47061214b054a5b75ef8e4e194ac03ea5
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-05-16 03:17:00 +0300

    add the edit route for product

--- routers/ProductRouters.js
+++ routers/ProductRouters.js
@@ -1,35 +1,46 @@
-import express from 'express';
-import multer from 'multer';
+import express from "express";
+import multer from "multer";
 import {
     createProduct,
     getAllProducts,
-    getLatestProducts, getProductsByBrand,
+    getLatestProducts,
+    getProductsByBrand,
     getProductsByCategory,
-    getTrendingProducts
+    getTrendingProducts,
+    updateProduct,
+    getProductsBySeller,
 } from "../controllers/ProductControllers.js";
-import {requireAuth} from "../middleware/RequireAuth.js";
+import { requireAuth } from "../middleware/RequireAuth.js";
 
 // const storage = multer.memoryStorage();
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         // Specify the directory where you want to save the files
-        cb(null, './uploads/'); // Make sure this directory exists
+        cb(null, "./uploads/"); // Make sure this directory exists
     },
     filename: function (req, file, cb) {
         // Define how the uploaded file should be named
-        const uniqueSuffix = Date.now() ;
-        cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
-    }
+        const uniqueSuffix = Date.now();
+        cb(
+            null,
+            file.fieldname +
+            "-" +
+            uniqueSuffix +
+            "." +
+            file.originalname.split(".").pop()
+        );
+    },
 });
 const upload = multer({ storage: storage });
 const product_router = express.Router();
 
-
-product_router.get("/latest", getLatestProducts)
-product_router.get("/trending",getTrendingProducts)
-product_router.get("/",getAllProducts)
-product_router.get("/:category",getProductsByCategory)
-product_router.get("/brand/:brand",getProductsByBrand)
-product_router.post("/add",requireAuth,upload.single('photo'), createProduct)
+product_router.get("/latest", getLatestProducts);
+product_router.get("/trending", getTrendingProducts);
+product_router.get("/", getAllProducts);
+product_router.get("/:category", getProductsByCategory);
+product_router.get("/seller/products", getProductsBySeller);
+product_router.get("/brand/:brand", getProductsByBrand);
+product_router.post("/add", requireAuth, upload.single("photo"), createProduct);
+product_router.put("/:id", requireAuth, upload.single("photo"), updateProduct);
 
 export default product_router;
